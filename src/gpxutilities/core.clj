7e379b894ee8fd5c9b70671ce0a0be1d71ef43fe
James Birnie <jimbobbirnie@gmail.com>
2017-08-10 11:52:30 +0100

    added some basic sanity checks for radian conversions
    James

--- src/gpxutilities/core.clj
+++ src/gpxutilities/core.clj
@@ -5,3 +5,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "This will become my gpx utilities project"))
+
+(defn distance-between [from to]
+  )
+
+(defn lat-distance-between [from to]
+  (Math/abs (- from to))
+  )
+
+(defn lon-distance-between [from to]
+  )
--- test/gpxutilities/core_test.clj
+++ test/gpxutilities/core_test.clj
@@ -2,7 +2,14 @@
   (:require [midje.sweet :refer :all]
             [gpxutilities.core :refer :all]))
 
-(facts "about the core functionality"
-  (fact "can measure the distance between points."
-  1 => 1
+(facts "about lat-distance-between"
+  (fact "is absolute value of from minus to"
+    (gpxutilities.core/lat-distance-between 1 2) => 1
+    (gpxutilities.core/lat-distance-between 3 2) => 1
   ))
+
+  (facts "about degrees and raidans"
+    (fact "basic conversions"
+    (java.lang.Math/toRadians 0) => 0.0
+    (java.lang.Math/toRadians 90) => (/ java.lang.Math/PI 2)
+    (java.lang.Math/toRadians 180) => java.lang.Math/PI))
